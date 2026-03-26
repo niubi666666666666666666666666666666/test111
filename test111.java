f8749e885fccb04c0f5cbeefb979ba19584c5acd
wuhaoyang.jofr <wuhaoyang.jofr@bytedance.com>
2026-03-26 17:34:31 +0800

    add sayHello

--- test111.java
+++ test111.java
@@ -4,4 +4,25 @@
  * @Description: version: 1.0
  */
 public class test111 {
+
+    // 打印问候语
+    public static void sayHello(String name) {
+        System.out.println("Hello, " + name + "! 今天也要好好写代码～");
+    }
+
+    // 计算两个整数的和
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        // 示例：打印一句话
+        sayHello("Haoyang");
+
+        // 示例：做个简单运算
+        int x = 3;
+        int y = 5;
+        int sum = add(x, y);
+        System.out.println(x + " + " + y + " = " + sum);
+    }
 }
